9a58ee51ab5a0b7231b0d817ec2675fe54647f1f
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-04-19 15:21:17 +0100

    Rewrite and refactor code in terms of small methods for giving feedback for a guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,33 +1,39 @@
+# frozen_string_literal: true
+
 # Initially I will code the function that gives feedback when a guess is made by the codebreaker
 # The colours will be represented by an array
-PEG_COLOURS = %w[A B C D E F]
+PEG_COLOURS = %w[A B C D E F].freeze
 # 6 possible peg colours and 12 guesses allowed for the codebreaker
-HINT_COLOURS = %w[Red White]
-p PEG_COLOURS
+HINT_COLOURS = %w[Red White].freeze
 # The first colour is for correct colour and position, second one for colour correct but in wrong position
 # The Code is an array of four peg_colours, as is the guess. The feedback is an array of Hint_Colours.
-def feedback(code, guess)
-    matched = Array.new(4, false)
-    not_exact_matches = []
-    output_array = []
-    4.times do |i|
-      if code[i] == guess[i]
-        matched[i] = true
-        output_array.push(HINT_COLOURS[0])
-      else
-        not_exact_matches.push(i)
-      end
-    end
-    # all exact matches have been found and the correct number of Red pegs given as feedback
-    not_exact_matches.each do |i|
-      not_exact_matches.each do |j|
-        if guess[i] == code[j] && matched[j] == false
-          matched[j] = true
-          output_array.push(HINT_COLOURS[1])
-        end
-      end
-    end
-    # we have pushed White pegs into the feedback output array for each new match which will be partial
-    output_array
+
+def exact_matches(array_one, array_two)
+  matches = 0
+  4.times do |i|
+    matches += 1 if array_one[i] == array_two[i]
+  end
+  matches
 end
-p feedback(%w(A C B A), %w(A D F E))
+
+def total_matches(array_one, array_two)
+  matches = 0
+  PEG_COLOURS.each do |i|
+    matches += [array_one.count(i), array_two.count(i)].min
+  end
+  matches
+end
+
+def feedback(code, guess)
+  output_array = Array.new(4)
+
+  (1..total_matches(code, guess)).each do |i|
+    output_array[i - 1] = HINT_COLOURS[1]
+  end
+  (1..exact_matches(code, guess)).each do |i|
+    output_array[i - 1] = HINT_COLOURS[0]
+  end
+  output_array
+end
+
+p feedback(%w[C B C C], %w[C C C B])
