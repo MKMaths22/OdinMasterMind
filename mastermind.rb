4b60b2e71a52b4b4ec62888a724edb0a64636bdc
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-04-19 15:26:28 +0100

    A few clarifying comments

--- mastermind.rb
+++ mastermind.rb
@@ -6,7 +6,8 @@ PEG_COLOURS = %w[A B C D E F].freeze
 # 6 possible peg colours and 12 guesses allowed for the codebreaker
 HINT_COLOURS = %w[Red White].freeze
 # The first colour is for correct colour and position, second one for colour correct but in wrong position
-# The Code is an array of four peg_colours, as is the guess. The feedback is an array of Hint_Colours.
+# The Code is an array of four peg_colours, as is the guess. Feedback is given as an array of length 4,
+# starting with the Hint_Colours pegs and completed with nil values
 
 def exact_matches(array_one, array_two)
   matches = 0
@@ -30,9 +31,11 @@ def feedback(code, guess)
   (1..total_matches(code, guess)).each do |i|
     output_array[i - 1] = HINT_COLOURS[1]
   end
+  # so we now have a White peg for every match which may or may not be an exact match
   (1..exact_matches(code, guess)).each do |i|
     output_array[i - 1] = HINT_COLOURS[0]
   end
+  # now we have changed White pegs into Red for each exact match and feedback is complete
   output_array
 end
 
